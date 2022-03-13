a9f0c134173ef82c478ddc7dfea5c49fda3faa4b
kiransadiq <kiransadiq054@gmail.com>
2022-03-13 23:22:18 +0500

    make netflix app

---
+++ src/Sdata.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+const Sdata=[
+    {
+      imgsrc:"https://i0.wp.com/therunneronline.com/wp-content/uploads/2021/10/SquidGameReviewImage.jpeg?fit=675%2C900&ssl=1",
+      title:"A netflix original series",
+      sname:"Squid Game",
+      links:"https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    },
+
+    {
+      imgsrc:"https://i0.wp.com/therunneronline.com/wp-content/uploads/2021/10/SquidGameReviewImage.jpeg?fit=675%2C900&ssl=1",
+      title:"A netflix original series",
+      sname:"Squid Game",
+      links:"https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    },
+
+
+    {
+      imgsrc:"https://i0.wp.com/therunneronline.com/wp-content/uploads/2021/10/SquidGameReviewImage.jpeg?fit=675%2C900&ssl=1",
+      title:"A netflix original series",
+      sname:"Squid Game",
+      links:"https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    },
+
+    {
+      imgsrc:"https://i0.wp.com/therunneronline.com/wp-content/uploads/2021/10/SquidGameReviewImage.jpeg?fit=675%2C900&ssl=1",
+      title:"A netflix original series",
+      sname:"Squid Game",
+      links:"https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    },
+
+    {
+      imgsrc:"https://i0.wp.com/therunneronline.com/wp-content/uploads/2021/10/SquidGameReviewImage.jpeg?fit=675%2C900&ssl=1",
+      title:"A netflix original series",
+      sname:"Squid Game",
+      links:"https://www.youtube.com/watch?v=oqxAJKy0ii4",
+    },
+   
+    
+
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<React.Fragment>
+  <h1 className="heading_style">List of top 5 netflix series in 2020</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<Card 
+  imgsrc={Sdata[0].imgsrc}
+  title={Sdata[0].title}
+  sname={Sdata[0].sname}
+  links={Sdata[0].links}
+
+/>
+<Card 
+  imgsrc={Sdata[1].imgsrc}
+  title={Sdata[1].title}
+  sname={Sdata[1].sname}
+  links={Sdata[1].links}
+
+/>
+<Card 
+  imgsrc={Sdata[2].imgsrc}
+  title={Sdata[2].title}
+  sname={Sdata[2].sname}
+  links={Sdata[2].links}
+
+/>
+
+<Card 
+  imgsrc={Sdata[3].imgsrc}
+  title={Sdata[3].title}
+  sname={Sdata[3].sname}
+  links={Sdata[3].links}
+
+/>
+
+<Card 
+  imgsrc={Sdata[4].imgsrc}
+  title={Sdata[4].title}
+  sname={Sdata[4].sname}
+  links={Sdata[4].links}
+
+/>
+</React.Fragment>,
+document.getElementById('root')
+);
